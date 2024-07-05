1344bfaff183021a0761ee6131874f67303fd801
Konstantin Tsybenkov <tsybenkov2005@gmail.com>
2024-07-05 16:35:12 +0300

    Returned the previous values for collectionResourceRel and path.

--- src/main/java/ru/ktsybenkov/tgBot/dao/OrderProductRepository.java
+++ src/main/java/ru/ktsybenkov/tgBot/dao/OrderProductRepository.java
@@ -4,8 +4,8 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.ktsybenkov.tgBot.entity.OrderProduct;
 
-@RepositoryRestResource(collectionResourceRel = "order_products"
-        , path = "order_products")
+@RepositoryRestResource(collectionResourceRel = "order_Products"
+        , path = "orderProducts")
 public interface OrderProductRepository
         extends JpaRepository<OrderProduct, Long> {
 }
