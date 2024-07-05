6a87b129d4bb039e8cc6c361b39127050c3ff5d0
Konstantin Tsybenkov <tsybenkov2005@gmail.com>
2024-07-05 16:37:35 +0300

    Fixed errors

--- src/main/java/ru/ktsybenkov/tgBot/dao/OrderProductRepository.java
+++ src/main/java/ru/ktsybenkov/tgBot/dao/OrderProductRepository.java
@@ -4,7 +4,7 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 import ru.ktsybenkov.tgBot.entity.OrderProduct;
 
-@RepositoryRestResource(collectionResourceRel = "order_Products"
+@RepositoryRestResource(collectionResourceRel = "orderProducts"
         , path = "orderProducts")
 public interface OrderProductRepository
         extends JpaRepository<OrderProduct, Long> {
